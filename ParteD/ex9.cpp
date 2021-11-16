d232cd0c813f2c7d1fabcab5a70ab25a026abea4
Ana Filipe <72576056+AnaaFilipe@users.noreply.github.com>
2021-11-16 20:38:14 +0000

    Ex9

---
+++ ParteD/ex9.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 3) {
+        cout << "ERROR!! Different number of inputs." << endl;
+        return -1;
+    }
+
+    // Read data of the file
+    Mat inimg = imread(argv[1]);
+    if(inimg.empty()) {
+        cout << "ERROR!! Couldn't open image." << endl;
+        return -1;
+    }
+
+    // Check parameters 
+    int option = -1;
+    while(option != 0 && option != 1){
+        cout << "Choose quantization option (0 for Mid-riser or 1 for Mid-tread): ";
+        cin >> option;
+    }
+
+    int Amax = 255, Amin = 0;
+    int nLevels = 6;
+    double delta = (Amax - Amin) / nLevels;
+
+    Mat outimg = Mat(inimg.rows, inimg.cols, inimg.type());
+    for (int i = 0; i < inimg.channels(); i++) {
+        for (int x = 0; x < inimg.cols; x++) {
+            for (int y = 0; y < inimg.rows; y++) {
+                if(option == 0) //Mid-riser quantization
+                    outimg.at<Vec3b>(y,x)[i] = delta * floor((inimg.at<Vec3b>(y, x)[i]/delta) + 0.5); 
+                else            //Mid-tread quantization
+                    outimg.at<Vec3b>(y, x)[i] = delta * (floor((inimg.at<Vec3b>(y, x)[i]/delta)) + 0.5);
+            }
+        }
+    }
+
+    imwrite("out.ppm",outimg);
+
+    imshow("Original Image", inimg);
+    imshow("Quantized Image", outimg);
+}
